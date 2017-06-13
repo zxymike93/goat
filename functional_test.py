3a214c45ea52e67f9c64b1ae5670adef6b35e57d
zxymike <zxymike93@gmail.com>
2017-06-13 22:35:08 +0800

    functional test as todo for project

--- functional_test.py
+++ functional_test.py
@@ -2,6 +2,21 @@ from selenium import webdriver
 
 
 browser = webdriver.Firefox(executable_path='/Applications/geckodriver')
+
+# 打开网站
 browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+# To-Do is browser title
+assert 'To-Do' in browser.title
+
+# entry
+
+# entry: "buy something"
+# update >> shows "1. buy something"
+
+# entry: "buy more"
+# update >> shows "2. buy more"
+
+# check
+
+browser.quit()
