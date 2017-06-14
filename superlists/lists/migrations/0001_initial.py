4d0ac823242f61cbfd96ebfee50dd74e1903329e
zxymike <zxymike93@gmail.com>
2017-06-14 23:56:09 +0800

    创建model for todo，单表，没有数据库关系。”

---
+++ superlists/lists/migrations/0001_initial.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Todo',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
+                ('task', models.TextField()),
+                ('created_time', models.DateTimeField(auto_now_add=True)),
+                ('updated_time', models.DateTimeField(auto_now=True)),
+            ],
+        ),
+    ]
--- superlists/lists/models.py
+++ superlists/lists/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Todo(models.Model):
+    task = models.TextField()
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.task
