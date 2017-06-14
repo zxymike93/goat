283b872d64ac98e99e8e436e2abe8efe483183cb
zxymike@acitsec <zxymike93@gmail.com>
2017-06-14 17:40:56 +0800

    unittest

--- functional_test.py
+++ functional_test.py
@@ -1,27 +1,39 @@
 import platform
+import unittest
 
 from selenium import webdriver
 
 
-if 'Ubuntu' in platform.platform():
-    browser = webdriver.Firefox(executable_path='/usr/local/bin/geckodriver')
-else:
-    browser = webdriver.Firefox(executable_path='/Applications/geckodriver')
+class NewVisitorTest(unittest.TestCase):
 
-# 打开网站
-browser.get('http://localhost:8000')
+    def setUp(self):
+        if 'Ubuntu' in platform.platform():
+            self.browser = webdriver.Firefox(
+                executable_path='/usr/local/bin/geckodriver')
+        else:
+            self.browser = webdriver.Firefox(
+                executable_path='/Applications/geckodriver')
+        self.browser.implicitly_wait(5)
 
-# To-Do is browser title
-assert 'To-Do' in browser.title
+    def tearDown(self):
+        """
+        Calls whether test succeed or not,
+        unless.. setUp() fails.
+        """
+        self.browser.quit()
 
-# entry
+    def test_can_start_and_entry_and_retrive_it_later(self):
+        self.browser.get('http://localhost:8000')
 
-# entry: "buy something"
-# update >> shows "1. buy something"
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('browser title is %s' % self.browser.title)
 
-# entry: "buy more"
-# update >> shows "2. buy more"
+        # entry: "buy something"
+        # update >> shows "1. buy something"
+        # entry: "buy more"
+        # update >> shows "2. buy more"
+        # check
 
-# check
 
-browser.quit()
+if __name__ == '__main__':
+    unittest.main()
