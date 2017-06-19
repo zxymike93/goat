657351fe0b3ce53f6a25618f527e41bf8e75eac4
zxymike@acitsec <zxymike93@gmail.com>
2017-06-19 17:56:43 +0800

    urls.py reconstruct

--- superlists/lists/urls.py
+++ superlists/lists/urls.py
@@ -2,8 +2,7 @@ from django.conf.urls import url
 
 
 urlpatterns = [
-    url(r'^$', 'lists.views.home_page', name='home_page'),
-    url(r'lists/new/$', 'lists.views.new_list', name='new_list'),
-    url(r'lists/(\d+)/$', 'lists.views.view_list', name='view_list'),
-    url(r'lists/(\d+)/add/$', 'lists.views.add_todo', name='add_todo'),
+    url(r'^new/$', 'lists.views.new_list', name='new_list'),
+    url(r'^(\d+)/$', 'lists.views.view_list', name='view_list'),
+    url(r'^(\d+)/add/$', 'lists.views.add_todo', name='add_todo'),
 ]
--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -1,14 +1,13 @@
 from django.conf.urls import include, url
 # from django.contrib import admin
 
-from lists import urls as lists_url
-
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'superlists.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^', include(lists_url)),
+    url(r'^$', 'lists.views.home_page', name='home_page'),
+    url(r'^lists/', include('lists.urls')),
     # url(r'^admin/', include(admin.site.urls)),
 ]
