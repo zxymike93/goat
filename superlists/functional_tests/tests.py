1608956078803a72b5ec2dd557def971b5dbb7bf
zxymike <zxymike93@gmail.com>
2017-06-20 22:31:53 +0800

    bootstrap to improve layout

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,14 +1,14 @@
 import platform
 import time
 
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 from utils import log
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     """LiveServerTestCase:
     Set up a live server for test and tear down when tests finished.
     """
