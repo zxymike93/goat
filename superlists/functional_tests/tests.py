1313179ba842b4d4acf7222868413d4c4955e049
zxymike <zxymike93@gmail.com>
2017-06-21 21:44:03 +0800

    Functional test can test whatever url.

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,4 +1,5 @@
 import platform
+import sys
 import time
 
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
@@ -12,6 +13,23 @@ class NewVisitorTest(StaticLiveServerTestCase):
     """LiveServerTestCase:
     Set up a live server for test and tear down when tests finished.
     """
+    @classmethod
+    def setUpClass(cls):
+        """Optionally set up url for liveserver-testing
+        >>> --liveserver=url
+        """
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://{}'.format(arg.split('=')[1])
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
+
     def setUp(self):
         self.browser = self.__chose_webdriver()
         self.browser.implicitly_wait(5)
@@ -38,7 +56,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         )
 
     def test_layout_and_styling(self):
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
 
         input = self.browser.find_element_by_id('id-input-todo')
@@ -57,7 +75,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         )
 
     def test_can_start_and_entry_and_retrive_it_later(self):
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
@@ -94,7 +112,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         # self.browser = webdriver.Firefox(
         #     executable_path='/Applications/geckodriver')
 
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
