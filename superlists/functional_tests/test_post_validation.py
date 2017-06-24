2fb20498f9be33a2b922de5787c692cde2a56b06
zxymike <zxymike93@gmail.com>
2017-06-24 10:14:41 +0800

    validation test

--- superlists/functional_tests/test_post_validation.py
+++ superlists/functional_tests/test_post_validation.py
@@ -1,20 +1,29 @@
-from unittest import skip
-
 from functional_tests.base import FunctionalTest
 
 
 class ValidationTest(FunctionalTest):
-    @skip
     def test_cannot_add_empty(self):
         # you post an empty input
-
+        self.browser.get(self.server_url)
+        self.browser.find_element_by_id('id-input-todo').send_keys('\n')
         # refresh the page
         # warning: no empty input
-
+        err = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(err.text, "You can't have an empty input")
         # enter something
         # ok
-
+        self.browser.find_element_by_id(
+            'id-input-todo').send_keys('Buy milk\n')
+        self._for_row_in_table('1: Buy milk')
         # post another empty input
         # warning again
+        self.browser.find_element_by_id('id-input-todo').send_keys('\n')
+        err = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(err.text, "You can't have an empty input")
+
+        self.browser.find_element_by_id(
+            'id-input-todo').send_keys('Make tea\n')
+        self._for_row_in_table('1: Buy milk')
+        self._for_row_in_table('2: Make tea')
 
         self.fail('Write me!')
