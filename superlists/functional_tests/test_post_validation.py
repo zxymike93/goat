3fe580742f7f6dcc6d0b5aafa686ea1e9e383390
zxymike <zxymike93@gmail.com>
2017-06-24 17:27:09 +0800

    use .send_keys(Keys.ENTER), .send_keys('\n') won't work

--- superlists/functional_tests/test_post_validation.py
+++ superlists/functional_tests/test_post_validation.py
@@ -1,28 +1,36 @@
+from selenium.webdriver.common.keys import Keys
+
 from functional_tests.base import FunctionalTest
 
 
 class ValidationTest(FunctionalTest):
-    def test_cannot_add_empty(self):
-        # you post an empty input
-        self.browser.get(self.server_url)
-        self.browser.find_element_by_id('id-input-todo').send_keys('\n')
-        # refresh the page
-        # warning: no empty input
-        err = self.browser.find_element_by_css_selector('.has-error')
-        self.assertEqual(err.text, "You can't have an empty input")
-        # enter something
-        # ok
-        self.browser.find_element_by_id(
-            'id-input-todo').send_keys('Buy milk\n')
-        self._for_row_in_table('1: Buy milk')
-        # post another empty input
-        # warning again
-        self.browser.find_element_by_id('id-input-todo').send_keys('\n')
-        err = self.browser.find_element_by_css_selector('.has-error')
-        self.assertEqual(err.text, "You can't have an empty input")
 
-        self.browser.find_element_by_id(
-            'id-input-todo').send_keys('Make tea\n')
+    def test_cannot_add_empty(self):
+        self.browser.get(self.server_url)
+        # you post an empty input
+        input = self.browser.find_element_by_id('id-input-todo')
+        input.send_keys('')
+        input.send_keys(Keys.ENTER)
+        # refreshed # warning: no empty input
+        err = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(err.text, "You can't have an empty input")
+        # if you enter something # ok
+        input = self.browser.find_element_by_id('id-input-todo')
+        input.send_keys('Buy milk')
+        input.send_keys(Keys.ENTER)
+        self._for_row_in_table('1: Buy milk')
+
+        # you're now at your list view
+        # post another empty input # warning again
+        input = self.browser.find_element_by_id('id-input-todo')
+        input.send_keys('')
+        input.send_keys(Keys.ENTER)
+        err = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(err.text, "You can't have an empty input")
+        # you enter something again # ok
+        input = self.browser.find_element_by_id('id-input-todo')
+        input.send_keys('Make tea')
+        input.send_keys(Keys.ENTER)
         self._for_row_in_table('1: Buy milk')
         self._for_row_in_table('2: Make tea')
 
